45761cd24abf924877169608265ea06d0b3bd9c2
numan067 <numan067@gmail.com>
2026-05-29 13:46:36 +0300

    Update MainActivity.kt

--- app/src/main/java/com/numan067/bordro/MainActivity.kt
+++ app/src/main/java/com/numan067/bordro/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.numan067.bordro.R // Bu satır hatayı düzeltecek olan kritik satır
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
